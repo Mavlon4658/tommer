199d8a4ca6c69ac616766e0c7c79ba5b9acdca13
Mavlon4658 <mavlon4658@gmail.com>
2023-12-10 11:42:23 +0500

    yangilandi

--- js/main.js
+++ js/main.js
@@ -42,19 +42,38 @@ let swiper2 = new Swiper(".project-slide", {
     },
 });
 
-let customer_slider = document.querySelector('#customers .customer_slider') 
+let customer_slider = document.querySelector('.customers .customer_slider') 
 if (customer_slider) {
     console.log('ishladi');
-    let custom_slide = new Swiper(customer_slider, {})
+    let custom_slide = new Swiper(customer_slider, {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".customer_slide_next_btn",
+            prevEl: ".customer_slide_prev_btn",
+        },
+    })
 }
 
 let swiper3 = new Swiper(".team-slide", {
     slidesPerView: 3,
     spaceBetween: 30,
-    freeMode: true,
-    loop: true,
+    // freeMode: true,
+    // loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
+
+let customer_sliders = document.querySelectorAll('.customers .customer_slider .swiper-slide')
+if (customer_sliders.length) {
+    customer_sliders.forEach(item => {
+        let btn = item.querySelector('.show__btn');
+        let description = item.querySelector('.description');
+        btn.onclick = () => {
+            description.classList.toggle('hide')
+            btn.classList.toggle('active')
+        }
+    })
+}
